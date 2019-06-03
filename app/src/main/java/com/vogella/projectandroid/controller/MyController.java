6b5a781d869edd126e9c71e9c04b383e137e9b45
ArthurLec <arthur.leclerc8@gmail.com>
2019-06-04 01:18:21 +0200

    first screen

---
+++ app/src/main/java/com/vogella/projectandroid/controller/MyController.java
@@ -0,0 +1,38 @@
+package com.vogella.projectandroid.controller;
+
+import com.vogella.projectandroid.RestApiGhibli;
+import com.vogella.projectandroid.model.Ghibli;
+import com.vogella.projectandroid.view.MainActivity;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MyController {
+    public MainActivity view;
+    private RestApiGhibli restApiGhibli;
+
+    public MyController(MainActivity view, RestApiGhibli restApiPokemon) {
+        this.view = view;
+        this.restApiGhibli = restApiPokemon;
+    }
+
+    public void onCreate() {
+        Call<List<Ghibli>> call = restApiGhibli.getGhibliData();
+        call.enqueue(new Callback<List<Ghibli>>() {
+            @Override
+            public void onResponse(Call<List<Ghibli>> call, Response<List<Ghibli>> response) {
+                List<Ghibli> restGhibliResponse = response.body();
+                List<Ghibli> ghibliList = restGhibliResponse;
+                view.showList(ghibliList);
+            }
+
+            @Override
+            public void onFailure(Call<List<Ghibli>> call, Throwable t) {
+
+            }
+        });
+    }
+}
